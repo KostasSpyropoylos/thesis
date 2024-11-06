131adf43b41c8d9c2718de2aac8de184358b401c
Konstantinos Spyropoulos <58692615+KostasSpyropoylos@users.noreply.github.com>
2024-11-06 20:39:27 +0200

    improved generated points

--- grid/generate-points.py
+++ grid/generate-points.py
@@ -3,19 +3,28 @@ import random
 class GeneratePoints:
     
     def __init__(self):
-        if os.path.isfile('points.txt'):
-            f = open("points.txt",'a')
+        if os.path.isfile('points.csv'):
+            f = open("points.csv",'a')
             
         else:
-            f = open("points.txt",'w')
-        for i in range(40):
+            f = open("points.csv",'w')
+            f.write(f"normLatitude,normLongitude\n")
+        for i in range(10000):
             x,y = self.generateRandomCoordinates()
             f.write(f"{x},{y}\n")   
         f.close()
     def generateRandomCoordinates(self):
-        x = random.uniform(0,5.93)
-        y = random.uniform(0,5.93)
+        x = self.non_uniform_random_1_to_3()
+        y = self.non_uniform_random_1_to_3()
         return x,y    
-
+    def non_uniform_random_1_to_3(self):
+        # Generate a random float uniformly between 1 and 3
+        uniform_random = random.uniform(1,2)
+        
+        # Apply a non-linear transformation to skew the distribution
+        # Here, we'll use a square function as an example
+        non_uniform = 1 + (uniform_random - 1) ** 2
+        
+        return non_uniform
             
 g = GeneratePoints()
